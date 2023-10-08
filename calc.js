5763133ebde4de38bf3a0ce4040a2dfb9de0b80a
EC2 Default User <ec2-user@ip-172-31-8-255.ap-northeast-1.compute.internal>
2023-10-08 11:00:47 +0000

    電卓

--- calc.js
+++ calc.js
@@ -1,10 +1,8 @@
 let display = document.getElementById('display');
 // let number = document.getElementsByClassName('number');
 let result = 0;
-let preValue =document.display.value.slice(-1);
+// let preValue =display.value.slice(-1);
 let num = document.getElementsByClassName('number');
-
-console.log(num);
 
 function clickButton(target) {
   // console.log(target);
@@ -19,71 +17,20 @@ function clickButton(target) {
     display.value = "0";
   } else if (target_value == "="){
     display.value = eval(result);
-  } else if (target_value==".") {
-    if (preValue == num) {
-      document.getElementById("comma").disabled ===false;
-    } else {
-      document.getElementById("comma").disabled ===true;
-    }
+  // } else if (target_value==".") {
+  //   if (preValue == num) {
+  //     document.getElementById("comma").disabled ===false;
+  //   } else {
+  //     document.getElementById("comma").disabled ===true;
+  //   }
   } 
   
   else {
     display.value += target_value;
   }
-  console.log(num);
-  // console.log(result);
+  console.log(result);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let display = document.getElementById('result');
-// let result = 0;
-
-// function clickButton(target) {
-//   console.log(target);
-  
-//   let target_value = target.innerHTML;
-//   console.log(target_value);
-  
-//   if (target_value == "AC") {
-//     display.value = "0";
-    
-//   } else if (target_value == "=") {
-    
-//     // result.value = eval("result.innerHTML");
-//     // console.log(result.innerHTML);
-//   } else {
-//     if (result.value == "0") {
-//       display.value = target_value;
-//     }else {
-//     // result.value += target_value;
-//     // }
-//       // console.log(result.value);
-//   }
-// }
 
 
 
