a8b0658e67092752eb624952969bae4f108f0fc6
EC2 Default User <ec2-user@ip-172-31-8-255.ap-northeast-1.compute.internal>
2023-10-09 05:07:16 +0000

    電卓

--- calc.js
+++ calc.js
@@ -1,35 +1,29 @@
 let display = document.getElementById('display');
 // let number = document.getElementsByClassName('number');
 let result = 0;
-// let preValue =display.value.slice(-1);
-let num = document.getElementsByClassName('number');
+// let preValue =document.display.value.slice(-1);
+// let num = document.getElementsByClassName('number');
+// let operator = ["+","-","*","/"];
 
 function clickButton(target) {
   // console.log(target);
   let target_value = target.innerHTML;
-  
-  // display.value += target_value;
-  result = display.value;
-  
-  // console.log(target_value);
+  result=display.value;
   
   if (target_value=="AC") {
-    display.value = "0";
+    display.value= "0";　//ACを押すと表示される数字を０に統一
   } else if (target_value == "="){
-    display.value = eval(result);
-  // } else if (target_value==".") {
-  //   if (preValue == num) {
-  //     document.getElementById("comma").disabled ===false;
-  //   } else {
-  //     document.getElementById("comma").disabled ===true;
-  //   }
+    display.value = eval(result); //計算結果を表示
+  } else if (display.value==="0" && target_value!==".") {
+    display.value = target_value;　//最初の０に上書き
   } 
-  
   else {
-    display.value += target_value;
+    display.value += target_value;　//入力したものを横に並べていく
   }
-  console.log(num);
+  console.log(result)
 }
+
+
 
 
 
