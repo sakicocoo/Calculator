1401c97378f42dd6df536a98d8dbbde2d035c9f7
EC2 Default User <ec2-user@ip-172-31-8-255.ap-northeast-1.compute.internal>
2023-10-09 05:40:39 +0000

    電卓

--- calc.js
+++ calc.js
@@ -16,11 +16,14 @@ function clickButton(target) {
     display.value = eval(result); //計算結果を表示
   } else if (display.value==="0" && target_value!==".") {
     display.value = target_value;　//最初の０に上書き
-  } 
+  } else if (target_value.slice(-1)==="="){
+    display.value="";
+    display.value=target_value;
+  }
   else {
     display.value += target_value;　//入力したものを横に並べていく
   }
-  console.log(result)
+  console.log(target_value);
 }
 
 
